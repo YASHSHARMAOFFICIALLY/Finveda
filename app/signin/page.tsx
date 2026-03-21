7afa20fcedf206ff608f425f0149ba157c9e88a6
Yash sharma <yashsharmaofficially@gmail.com>
2026-03-21 15:39:16 +0530

    fixed error

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -96,7 +96,7 @@ function SignInForm() {
                         <div className="space-y-2">
                             <div className="flex items-center justify-between">
                                 <Label htmlFor="password">Password</Label>
-                                <Link href="/forgot-password" size="sm" className="text-xs text-primary hover:underline">
+                                <Link href="/forgot-password" className="text-xs text-primary hover:underline">
                                     Forgot password?
                                 </Link>
                             </div>
